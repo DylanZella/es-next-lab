58d3f13c8b91cd81a06618fc72e3284c145f38b7
DylanZella <1018610794@qq.com>
2021-10-22 23:49:06 +0800

    update some config

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,26 @@
-
+<template>
+  <div>
+    <Header></Header>
+    <Navigator></Navigator>
+  </div>
+</template>
 
 <script>
-
+import Header from './components/Header.vue'
+import Navigator from './components/Navigator.vue'
 export default {
   name: 'App',
-
+  components: {
+    Header,
+    Navigator
+  }
 }
 </script>
 
 <style>
-
+body {
+  margin: 0;
+  height: 100%;
+  width: 100%;
+}
 </style>
---
+++ src/components/header.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="common-layout">
+    <el-container>
+      <el-header class="fixed w-full bg-white shadow-sm border-b-1 flex">
+        <span>ES6-lab</span>
+        <el-icon>
+          <connection />
+        </el-icon>
+      </el-header>
+    </el-container>
+  </div>
+</template>
+
+<style>
+.el-header,
+.el-footer {
+  background-color: #b3c0d1;
+  color: var(--el-text-color-primary);
+  text-align: center;
+  line-height: 60px;
+}
+
+.el-aside {
+  background-color: #d3dce6;
+  color: var(--el-text-color-primary);
+  text-align: center;
+  line-height: 200px;
+}
+
+.el-main {
+  background-color: #e9eef3;
+  color: var(--el-text-color-primary);
+  text-align: center;
+  line-height: 160px;
+}
+
+body > .el-container {
+  margin-bottom: 40px;
+}
+
+.el-container:nth-child(5) .el-aside,
+.el-container:nth-child(6) .el-aside {
+  line-height: 260px;
+}
+
+.el-container:nth-child(7) .el-aside {
+  line-height: 320px;
+}
+
+</style>
