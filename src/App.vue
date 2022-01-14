ce2e22363b10ca357eac8cc333d0f88061a39a39
dylan mei <dylan.m.xin@gamil.com>
2022-01-14 18:10:42 +0800

    update home page

--- src/App.vue
+++ src/App.vue
@@ -2,8 +2,6 @@
   <NConfigProvider :theme="store.state.theme" :locale="store.state.locale">
     <!-- <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" /> -->
     <Home />
-    <NButton @click="store.state.theme = darkTheme">深色</NButton>
-    <NButton @click="store.state.theme = null">浅色</NButton>
   </NConfigProvider>
 </template>
 
@@ -35,6 +33,5 @@
     -moz-osx-font-smoothing: grayscale;
     text-align: center;
     color: #2c3e50;
-    margin-top: 60px;
   }
 </style>
--- src/components/header.vue
+++ src/components/header.vue
@@ -1,12 +1,23 @@
 <template>
-  <div class="header-container">
-    <div>HelloWorld</div>
-    <div>bar</div>
-    <div>next</div>
+  <div class="header-container shadow-sm justify-between">
+    <a class="header-logo flex items-center" href="/">
+      <img src="@/assets/site-logo.png" alt="logo" />
+      <span class="text-lg ml-1">语法实验室</span>
+    </a>
+    <div class="flex items-center">
+      <a href="https://github.com/DylanZella/es-next-lab">
+        <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 24 24" class="w-7 h-7"><path d="M9 19c-4.3 1.4-4.3-2.5-6-3m12 5v-3.5c0-1 .1-1.4-.5-2c2.8-.3 5.5-1.4 5.5-6a4.6 4.6 0 0 0-1.3-3.2a4.2 4.2 0 0 0-.1-3.2s-1.1-.3-3.5 1.3a12.3 12.3 0 0 0-6.2 0C6.5 2.8 5.4 3.1 5.4 3.1a4.2 4.2 0 0 0-.1 3.2A4.6 4.6 0 0 0 4 9.5c0 4.6 2.7 5.7 5.5 6c-.6.6-.6 1.2-.5 2V21" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path></svg>
+      </a>
+      <span class="text-xs text-gray-500 ml-3">
+        V 0.1.0
+      </span>
+    </div>
   </div>
 </template>
 
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+  import Breadcrumb from '@/components/Breadcrumb.vue';
+</script>
 
 <style lang="scss" scoped>
   .header-container {
@@ -16,6 +27,16 @@
     width: 100vw;
     display: flex;
     padding: 0 2rem;
-    background-color: cornsilk;
+    background-color: white;
+    align-items: center;
+  }
+  .header-logo {
+    & > span {
+      color: #e99547;
+      font-weight: 600;
+    }
+    & > img {
+      height: 2rem;
+    }
   }
 </style>
